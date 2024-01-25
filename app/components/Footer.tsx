8f8aec7c60a0f42dc8d0f515b6a645484b1f2b6e
joaoleandro <new.joaoleandro@gmail.com>
2024-01-25 15:38:27 -0300

    initial commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Container from "./Container";
+
+const Footer = () => {
+    return ( 
+        <footer>
+            <Container>
+                Esse é meu footer
+            </Container>
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ app/components/Header.tsx
@@ -0,0 +1,13 @@
+import Container from "./Container";
+
+const Header = () => {
+    return ( 
+        <header>
+            <Container>
+                Esse é meu header
+            </Container>
+        </header>
+     );
+}
+ 
+export default Header;
